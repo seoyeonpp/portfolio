7afa7339cf65546f03348a4b29e03fded5f509ce
seoyeonpp <seoyeonpub@gmail.com>
2021-05-05 23:49:56 +0900

    mainvisual수정

--- js/script.js
+++ js/script.js
@@ -77,14 +77,6 @@ $(function () {
             })
         })
     }
-
-    // 메뉴호버효과
-    var hover = $('section h2');
-    hover.mousemove(function (e) {
-        var rXP = (e.pageX - this.offsetLeft - $(this).width() / 2);
-        var rYP = (e.pageY - this.offsetTop - $(this).height() / 2);
-        hover.css('text-shadow', +rYP / 10 + 'px ' + rXP / 80 + 'px rgba(213, 82, 57,0.8), ' + rYP / 8 + 'px ' + rXP / 60 + 'px rgba(228,186,125,1), ' + rXP / 70 + 'px ' + rYP / 12 + 'px rgba(146,171,163,.7)');
-    });
 });
 
 // 탑버튼
